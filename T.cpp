a68e6b6132ac09360a10412dc9e730ca2ad4c11a
VokasCuey <1374361804@qq.com>
2021-08-05 16:47:06 +0800

    Ex5 solved

--- T.cpp
+++ T.cpp
@@ -1,62 +1,25 @@
 #include <iostream>
 #include <stdlib.h>
-#include <math.h>
 using namespace std;
 
-bool Get(int x);
-int maxNum(int n[4]);
-int minNum(int n[4]);
-int Run(int x);
+int test(int n[4]);
 
 int main()
 {
-    int x = 0;
-    if (!Get(x))
-    {Get(x);}
-    Run(x);
+    int n[4]={0};
+    test(n);
+    for (int i = 0; i < 4; i++)
+    {
+        cout << n[i] << endl;
+    }
     system("pause");
     return 0;
 }
-bool Get(int x)
+int test(int n[4])
 {
-    cout << "Please enter the value of integer x(999<x<10000)." << endl;
-    cin >> x;
-    if ((x>999)&&(x<10000))
-    {return true;}
-    else
-    {return false;}
-}
-int maxNum(int n[4])
-{
-    int maxNum = 0;
-    int Max[4] = {0};
-    for (int i = 3; i >= 0; i--)
+    for (int i=0;i<4;i++)
     {
-        for (int j = 0; j <= i; j++)
-        {
-            if (n[j] > Max[i])
-            {
-                Max[i] = n[j];
-            }
-        }
-        maxNum = maxNum + Max[i] * pow(10, i);
+        n[i]=i;
     }
-    cout << "The max recofigurated integer is " << maxNum << endl;
-    return maxNum;
-}
-int Run(int x)
-{
-    int n[4] = {0};
-    for (int i = 3; i >= 0; i--)
-    {
-        n[i] = x;
-        for (int j = i + 1; j < 4; j++)
-        {
-            n[i] = n[i] - n[j] * pow(10, j);
-        }
-        n[i] = n[i] / int(pow(10, i));
-        cout << n[i] << " ";
-    }
-    cout << endl;
     return 0;
 }
--- section5/Ex5.cpp
+++ section5/Ex5.cpp
@@ -12,21 +12,44 @@ int main()
     int x = 0;
     cout << "Please enter the value of integer x(999<x<10000)." << endl;
     cin >> x;
-    Run(x);
+    for (int i = 1;; i++)
+    {
+        int r = Run(x);
+        if (r != x)
+        {
+            x = r;
+            cout << "Operation " << i << endl << endl;
+        }
+        else
+        {
+            cout << endl << "Operation end." << endl << endl;
+            break;
+        }
+    }
+    cout << "The final result is " << x << endl;
     system("pause");
     return 0;
 }
 int maxNum(int n[4])
 {
+    int temp[4] = {n[0], n[1], n[2], n[3]};
     int maxNum = 0;
     int Max[4] = {0};
     for (int i = 3; i >= 0; i--)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = 0; j < 4; j++)
         {
-            if (n[j] > Max[i])
+            if (temp[j] > Max[i])
             {
-                Max[i] = n[j];
+                Max[i] = temp[j];
+            }
+        }
+        for (int j = 0; j < 4; j++)
+        {
+            if (temp[j] == Max[i])
+            {
+                temp[j] = 0;
+                break;
             }
         }
         maxNum = maxNum + Max[i] * pow(10, i);
@@ -34,8 +57,43 @@ int maxNum(int n[4])
     cout << "The max recofigurated integer is " << maxNum << endl;
     return maxNum;
 }
+int minNum(int n[4])
+{
+    int temp[4] = {n[0], n[1], n[2], n[3]};
+    int minNum = 0;
+    int Min[4] = {9, 9, 9, 9};
+    for (int i = 3; i >= 0; i--)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (temp[j] < Min[i])
+            {
+                if ((i == 3) && (temp[j] == 0))
+                {
+                    continue;
+                }
+                else
+                {
+                    Min[i] = temp[j];
+                }
+            }
+        }
+        for (int j = 0; j < 4; j++)
+        {
+            if (temp[j] == Min[i])
+            {
+                temp[j] = 9;
+                break;
+            }
+        }
+        minNum = minNum + Min[i] * pow(10, i);
+    }
+    cout << "The min recofigurated integer is " << minNum << endl;
+    return minNum;
+}
 int Run(int x)
 {
+    int r = 0;
     int n[4] = {0};
     for (int i = 3; i >= 0; i--)
     {
@@ -45,9 +103,7 @@ int Run(int x)
             n[i] = n[i] - n[j] * pow(10, j);
         }
         n[i] = n[i] / int(pow(10, i));
-        cout << n[i] << " ";
     }
-    cout << endl;
-    maxNum(n);
-    return 0;
+    r = maxNum(n) - minNum(n);
+    return r;
 }
