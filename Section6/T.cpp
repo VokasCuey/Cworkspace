0f33091b68e56d6264440b741927d38012eeba6c
VokasCuey <1374361804@qq.com>
2021-08-17 00:32:34 +0800

    test

--- Section6/T.cpp
+++ Section6/T.cpp
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 using namespace std;
 
-typedef void (*fun)();
-void f1();
-void f2();
-void f3();
-void f4();
-fun p[]={f1,f2,f3,f4};
-
-
 int main()
 {
-    system ("pause");
+    float avgLake = 0.0;
+    int Lake[5][5];
+    int(*p)[5] = Lake;
+    int *t = &Lake[4][4];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            Lake[i][j] = i * j + j;
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << p[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << *(*(Lake + i) + j) << " ";
+        }
+        cout << endl;
+    }
+    do
+    {
+        cout << *t << endl;
+        t--;
+    } while (t >= Lake[0]);
+    system("pause");
     return 0;
 }
