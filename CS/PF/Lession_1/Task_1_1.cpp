06e82895092e4b0291191b213f7d810315a2f879
VokasCuey <1374361804@qq.com>
2021-09-14 22:27:12 +0800

    task_1_1 finished

--- CS/PF/Lession_1/Task_1_1.cpp
+++ CS/PF/Lession_1/Task_1_1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstdlib>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World.\n";
+    cout << "清华大学 程序设计基础 7.7 技科楼3311" << endl;
+    printf("%s\n","清华大学 程序设计基础 7.7 技科楼3311");
     system("pause");
     return 0;
 }
